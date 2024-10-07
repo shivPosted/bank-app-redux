44ab650706a09a8ba6e96017f7fe844cb5b6e794
shivPosted <shivpratap621@gmail.com>
2024-10-07 20:47:44 +0530

    testing connection with redux on react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
-function App() {}
+import { useSelector } from "react-redux";
+
+function App() {
+  const customer = useSelector((store) => store.customer);
+  return <div>Hello {customer.fullName}</div>;
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import store from "./myStore.js";
+import { Provider } from "react-redux";
+import { createCustomer } from "./features/customers/customerSlice.js";
 
 store.dispatch({ type: "account/deposit", payload: 5000 });
 console.log(store.getState());
+store.dispatch(createCustomer("Shiv Pratatp Singh", 125364));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
